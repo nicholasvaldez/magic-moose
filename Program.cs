df965dfc613a70df6c85b9ff9360abf30bd896cf
Nick Valdez <nicholastvaldez@gmail.com>
2023-08-23 13:23:48 -0500

    add logic for returning a random response

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 Console.WriteLine("Welcome to The Magic Moose Fortune Telling Experience!");
 Console.WriteLine("------------------------------------------------------");
@@ -36,3 +37,36 @@ void MooseSays(string message)
 }
 
 MooseSays("Ask me anything!");
+Console.WriteLine("> ");
+Console.ReadLine();
+
+string[] responses = new string[]
+{
+    "As I see it, yes.",
+    "Ask again later.",
+    "Better not tell you now.",
+    "Cannot predict now.",
+    "Concentrate and ask again.",
+    "Don't count on it.",
+    "It is certain.",
+    "It is decidedly so.",
+    "Most likely.",
+    "My reply is no.",
+    "My sources say no.",
+    "Outlook not so good.",
+    "Outlook good.",
+    "Reply hazy, try again.",
+    "Signs point to yes.",
+    "Very doubtful.",
+    "Without a doubt.",
+    "Yes.",
+    "Yes- definitely.",
+    "You may rely on it."
+
+};
+
+Random r = new Random();
+int randomIndex = r.Next(0, responses.Length);
+
+string randomResponse = responses[randomIndex];
+MooseSays(randomResponse);
